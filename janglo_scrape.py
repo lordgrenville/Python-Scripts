88dea1f7b52453da11835a0d65a92608002952e2
Josh Friedlander <joebobson87@yahoo.com>
2017-12-17 19:05:55 +0200

    removed superfluous column and converts string date to datetime object

--- janglo_scrape.py
+++ janglo_scrape.py
@@ -1,13 +1,20 @@
 import urllib.request
 from bs4 import BeautifulSoup
-#http://www.janglo.net/index.php?option=com_adsmanager&page=show_category&catid=77&order=0&expand=-1&Itemid=160
-janglo = "http://www.janglo.net/index.php?option=com_adsmanager&page=show_category&catid=77&order=0&expand=-1&Itemid=160" \
-         "ad_jobtag,/mn_adprice,0/mx_adprice,0/ad_nrooms,/expand,1/Itemid,160/limit,20/limitstart,20/"
 
-page = urllib.request.urlopen(janglo)
 
-soup = BeautifulSoup(page, "html.parser")
-all_links = soup.find_all("a")
-for link in all_links:
-    if link.get("href")[:31] == 'index.php?option=com_adsmanager':
-        print (link.get_text().rstrip())
+def janglo_scraper():
+    janglo = "http://www.janglo.net/index.php?option=com_adsmanager&page=show_category&catid=77&order=0&expand=-1" \
+             "&Itemid=160ad_jobtag,/mn_adprice,0/mx_adprice,0/ad_nrooms,/expand,1/Itemid,160/limit,20/limitstart,20/"
+
+    page = urllib.request.urlopen(janglo)
+    jobs = []
+    soup = BeautifulSoup(page, "html.parser")
+    all_links = soup.find_all("a")
+    for link in all_links:
+        if link.get("href")[:31] == 'index.php?option=com_adsmanager':
+            jobs.append(link.get_text().rstrip())
+    return jobs
+
+
+if __name__ == '__main__':
+    janglo_scraper()
--- secret_scrape.py
+++ secret_scrape.py
@@ -1,73 +1,83 @@
-#TODO add janglo jobs to here and make cron job to update this once a day
-#TODO don't add duplicate records!
+# TODO add janglo jobs to here and make cronjob to update this once a day
+# problem - janglo has unpredictable size, how do you differentiate jobs?
+# cronjob can be done in windows with task scheduler
+# TODO don't add duplicate records! namely, add a primary key
+# in the meantime can use this
+# SELECT DISTINCT Title, Company FROM jobs WHERE Date BETWEEN datetime('now', '-3 days') AND datetime('now', 'localtime');
 
 import csv
 import datetime
 import urllib.request
 import sqlite3
-# from sqlite3 import Error
+from sqlite3 import Error
 from bs4 import BeautifulSoup
+from dateutil import parser
 
 """
 Scrapes Secret Tel Aviv jobs board, adds new jobs into a database
 """
 
+
 def update_db():
-    """
-create a database connection and save it
-    """ 
     try:
         conn = sqlite3.connect('jobs.db')
-        c=conn.cursor()
+        c = conn.cursor()
+
+        # call the scraping functions
+        soup = scrape_secret()
+        jobs = clean_jobs(soup)
+        result = organise(jobs)
+        final = data_cleanser(result)
+        export(final)
+
+        # after exporting to csv (just in case) we delete the title row and convert nested lists to tuples
+        del final[0]
+        new_result = [tuple(l) for l in final]
+        # only necessary once
+        # c.execute('''CREATE TABLE jobs (Title, Company, Location, Type, Date Posted)''')
+
+        c.executemany("INSERT INTO jobs VALUES (?,?,?,?,?)", new_result)
+        conn.commit()
+        conn.close()
+
     except Error as e:
         print(e)
-    
-    #call the scraping functions
-    soup = scrape_secret()
-    jobs = clean_jobs(soup)
-    result = organise(jobs)
-    export(result)
 
-    #after exporting to csv (just in case) we delete the title row and convert nested lists to tuples
-    del result[0]
-    new_result = [tuple(l) for l in result]
-    # only necessary once
-    # c.execute('''CREATE TABLE jobs (Title, Company, Location, Duplicate, Type, Date Posted)''')
 
-    c.executemany("INSERT INTO jobs VALUES (?,?,?,?,?,?)", new_result)
-    conn.commit()
-    conn.close()
- 
-#function to remove multiple occurences of one term ('new')
-def remove_values_from_list(the_list, val):
+# function to remove multiple occurrences of one term ('new')
+def remove_value_from_list(the_list, val):
     return [value for value in the_list if value != val]
+
 
 def length_enforcer(the_list, length):
     return [value for value in the_list if len(value) == length]
 
-#hit the website and scrape the first page
+
+# hit the website and scrape the first page
 def scrape_secret():
-    URL = "https://jobs.secrettelaviv.com/"
-    req = urllib.request.Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+    url = "https://jobs.secrettelaviv.com/"
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     page = urllib.request.urlopen(req)
     return BeautifulSoup(page, "html.parser")
 
+
 def clean_jobs(soup):
-    #jobs are in 'spans'
+    # jobs are in 'spans'
     all_spans = soup.find_all("span")
     jobs = []
     for span in all_spans:
         jobs.append(span.get_text().strip())
-    #remove extraneous elements
+    # remove extraneous elements
     jobs.remove('')
     jobs.remove('Subscribe to our EVENTS Newsletter')
     jobs.remove('Join our facebook GROUP')
-    jobs = remove_values_from_list(jobs, '')
-    return remove_values_from_list(jobs, 'new')
+    jobs = remove_value_from_list(jobs, '')
+    return remove_value_from_list(jobs, 'new')
+
 
 def organise(jobs):
-    #make list of lists
-    result = [["Title","Company","Location","Duplicate", "Type", "Date Posted"]]
+    # make list of lists
+    result = [["Title", "Company", "Location", "Duplicate", "Type", "Date Posted"]]
     new_list = []
     for job in jobs:
         if len(new_list) == 6:
@@ -77,15 +87,25 @@ def organise(jobs):
         else:
             new_list.append(job)
     result.append(new_list)
-    result = length_enforcer(result,6)
+    return length_enforcer(result, 6)
+
+
+def data_cleanser(result):
+    for i in result:
+        del i[3]
+        try:
+            i[4] = parser.parse(i[4])
+        except ValueError:
+            pass
     return result
 
+
 def export(result):
-    #export as CSV
     csvfile = "secret_today" + datetime.datetime.today().strftime('%m-%d') + ".csv"
     with open(csvfile, "w") as output:
         writer = csv.writer(output, lineterminator='\n')
         writer.writerows(result)
+
 
 if __name__ == '__main__':
     update_db()
